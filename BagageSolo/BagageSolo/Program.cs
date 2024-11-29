c8378cb676650b12a3905d7fb5b2765b4438c4c0
Sam Bolluijt <149382099+d296063@users.noreply.github.com>
2024-11-29 09:27:09 +0100

    added start up file

---
+++ BagageSolo/BagageSolo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace BagageSolo;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welkom bij zwembad de golf!\n");
+        //stop hier je hoofd loop
+
+        //laat het menu zien
+        // Console.WriteLine("1. Laat het aantal kluizen zien.");
+        //etc.
+
+    }
+    static int LockerCount()
+    {
+        int count = 0;
+        //vind aantal
+        return count;
+    }
+    static int NewLocker()
+    {
+        //logica om nieuwe kluis te openen
+        return 0;
+    }
+    static bool OpenLocker()
+    {
+        //logica om  kluis te openen
+        return false;
+    }
+    static bool ReturnLocker()
+    {
+        //logica om  kluis permanent te openen
+        return false;
+    }
+    //helper functie template voor het uitlezen van het bestand
+    static List<Kluis> GetLockersFromFile()
+    {
+        return [];
+    }
+    //helper functie template voor het schrijven van het bestand
+    static void SaveLockersToFile(List<Kluis> lockers)
+    {
+        //logica om lockers te schrijven naar bestand
+    }
+}
